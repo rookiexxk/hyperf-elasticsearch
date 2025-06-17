c77bb513ae694a9e223e337011b202c9fe18884b
weijiang <weijiang@addcn.com>
2025-06-17 18:25:29 +0800

    添加聚合查询

--- src/Builder.php
+++ src/Builder.php
@@ -640,13 +640,6 @@ class Builder
         return $this;
     }
 
-    /**
-     * 添加聚合查询
-     *
-     * @param string $name 聚合名称
-     * @param array $aggregation 聚合定义
-     * @return $this
-     */
     public function addAggs(string $name, array $aggregation): Builder
     {
         $this->aggs[$name] = $aggregation;
